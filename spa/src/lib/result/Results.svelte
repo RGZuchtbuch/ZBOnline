964bc5f2a3e1537dfeb4805c4c07715c6892f005
eelco.jannink <e.h.a.jannink@saxion.nl>
2024-04-12 09:13:31 +0200

    Solved scroll bug in Reports

--- spa/src/lib/result/Results.svelte
+++ spa/src/lib/result/Results.svelte
@@ -141,7 +141,7 @@
 
 
 
-<div>
+<!--div -->>
     <div class='w-256 flex no-print'>
         <h2 class='grow text-center text-2xl print'>Zuchtleistungen</h2>
         <div class='w-8 justify-center m-2 circled bg-alert text-white cursor-pointer no-print' on:click={onHelp} title='Anleitung'>?</div>
@@ -250,6 +250,6 @@
     {#if help}
         <Help on:help={onHelp} />
     {/if}
-</div>
+<!--/div-->
 
 
