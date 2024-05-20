9eb89189e723d76b3b0c056cdf23091f37dc3a22
eelco.jannink <e.h.a.jannink@saxion.nl>
2024-05-20 11:49:30 +0200

    Kept working on Grading, now calculations is ok, needs work on layout and print.

--- spa/src/lib/result/Grading.svelte
+++ spa/src/lib/result/Grading.svelte
@@ -3,12 +3,12 @@
 	import grading from '../../js/aab.js';
 	import {dec} from '../../js/util.js';
 	import { standard } from '../../js/store.js'
+
 	import Form from '../common/form/Form.svelte';
 	import Select from '../common/form/input/Select.svelte';
     import NumberInput from '../common/form/input/NumberInput.svelte';
     import TextInput from '../common/form/input/TextInput.svelte';
 	import DateInput from '../common/form/input/DateInput.svelte';
-	import Text from '../read/Text.svelte';
 	import RingInput from '../common/form/input/RingInput.svelte';
 
 	const route = meta();
@@ -17,31 +17,49 @@
     let breeds = null;
 	let colors = null;
 
-    let hasLayEggs = null;
+	let pair = null;
 
-    let hasBroods = null; // pigeons
-    let hasBroodEggs = null; // layers
-    let hasBroodChicks = null; // both
+	createData();
+
+	function createData() {
+		pair = {
+			number: null,
+			date: null, show: null,
+			breeder: null,
+			section: null, breed: null, color: null,
+			grade: '?'
+		}
+		clearResults();
+	}
+
+	function clearResults() {
+		pair.animals = [];
+		if( pair.section ) {
+			const max = pair.section.id === PIGEONS ? 2 : 3;
+			for (let i = 0; i < max; i++) {
+				pair.animals.push({
+					ring: null, grade: '?',
+					parents: [
+						{ sex:'1.0', ring: null, brood: {eggs: null, chicks: null, grade: '?'}},
+						{ sex:'0.1', ring: null, lay: {eggs: null, grade: '?'}, brood: {eggs: null, hatched: null, grade: '?'}},
+					],
+				});
+			}
+		}
+	}
 
     function updateBreeds( event ) {
-		const section = certificate.section;
+		const section = pair.section;
         if( section && section.id > 0 ) {
-
 			breeds = [];
             const foundSection = findSection( section.id, $standard );
             if( foundSection ) {
                 collectBreeds(foundSection, breeds);
                 breeds.sort( (a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0 );
             }
+			clearResults();
         }
     }
-
-	function updateValues( event ) {
-		hasLayEggs = null;
-		hasBroods = null;
-		hasBroodEggs = null;
-		hasBroodChicks = null;
-	}
 
     function findSection( id, section ) { // find section to id
         if( section.id === id ) return section;
@@ -61,58 +79,88 @@
     }
 
 	function gradeLay( lay ) {
-		console.log( 'Lay', lay );
-
-		lay.grade = certificate.breed && certificate.breed.layEggs && lay && lay.eggs > 0 ? grading.lay(lay.eggs, certificate.breed.layEggs) : '?';
-		certificate = certificate;
+		console.log( 'Pair', pair );
+		lay.grade = pair.breed && pair.breed.layEggs > 0 && lay && lay.eggs > 0 ? grading.lay(lay.eggs, pair.breed.layEggs) : '?';
+		recalculateGrades();
 	}
 
 	function gradeBrood( brood ) {
-		console.log( 'Brood', brood );
-		brood.grade = brood.eggs > 0 && brood.hatched > 0 ? grading.brood.layers( brood.eggs, brood.hatched ) : null;
-		certificate = certificate;
+		brood.grade = brood && brood.eggs > 0 && brood.hatched > 0 ? grading.brood.layers(brood.eggs, brood.hatched) : '?';
+		recalculateGrades();
 	}
 
-	let certificate = { // layers
+	function recalculateGrades() {
+		console.log( 'grade pair' );
 
-		number: null,
-		date: null, show: null,
-		breeder: null,
-		section: null, breed:null, color:null,
+		if( pair.section && pair.breed ) {
+			for( const animal of pair.animals ) {
+				if( pair.section.id === PIGEONS ) {
+					if( animal.parents[0].brood.grade !== '?' && animal.parents[1].brood.grade !== '?' ) {
+						animal.grade = ( animal.parents[0].brood.grade + animal.parents[1].brood.grade ) / 2.0;
+					} else {
+						animal.grade = '?';
+					}
+					console.log( 'Todo'); // TODO
+				} else { // layer
+					if( animal.parents[0].brood.grade !== '?' && animal.parents[1].brood.grade !== '?' && animal.parents[1].lay.grade !== '?' ) {
+						animal.grade = (animal.parents[0].brood.grade + animal.parents[1].brood.grade + animal.parents[1].lay.grade) / 3.0;
+					} else {
+						animal.grade = '?';
+					}
+				}
+			}
 
-		sire: {
-			ring: null,
-			parents: {
-				sire: {ring: null, brood: {eggs: null, chicks: null, grade:null} },
-				dame: {ring: null, lay: {eggs: null, grade:null}, brood: {eggs: null, hatched: null, grade:null },},
+			if( pair.animals.every( (animal) => animal.grade !== '?' ) ) {
+				console.log('Pair grade', pair, pair.animals.length );
+				pair.grade = pair.animals.reduce( ( sum, current ) => sum + current.grade, 0 ) / pair.animals.length;
+				return; // success
 			}
-		},
-		dames: [
-			{
-				ring: null,
-				parents: {
-					sire: {ring: null, brood: {eggs: null, chicks: null, grade:null} },
-					dame: {ring: null, lay: {eggs: null, grade:null}, brood: {eggs: null, hatched: null, grade:null },},
-				},
-			}, {
-				ring: null,
-				parents: {
-					sire: {ring: null, brood: {eggs: null, chicks: null, grade:null} },
-					dame: {ring: null, lay: {eggs: null, grade:null}, brood: {eggs: null, hatched: null, grade:null },},
-				},
-			}
-		]
+		}
+		pair.grade = '?';
 	}
+
+
+	function gradeAnimal( animal ) {
+		animal.grade = Math.round( (animal.parents['1.0'].brood.grade + animal.parents['0.1'].brood.grade + 2 * animal.parents['0.1'].lay.grade ) / 4 );
+		gradePair();
+		return animal.grade;
+	}
+
+	function gradePair() {
+		console.log( 'grade pair' );
+		const animals = [ pair.sire, pair.dames[0], pair.dames[1] ];
+		let sum = 0;
+		for( const animal of animals ) {
+			if( animal.grade === '?' ) return '?';
+			sum += animal.grade;
+		}
+		pair.grade = Math.round( sum / animals.length );
+	}
+
+	function grade( value ) {
+		return value === '?' ? '?' : value.toFixed(1);
+	}
+
 </script>
 
-<article class='w-256'>
+<main class='w-256 p-4 bg-gray-100'>
 	<Form autoSave={false}>
 		<header>
 			<h1 class='text-center'>BDRG Zuchtbuch</h1>
-			<h1 class='text-center'>Abstammungsnachweis {#if certificate.section} {certificate.section.name} {/if}</h1>
+
+			<div class='flex flex-row px-2 justify-center'>
+				<h3 class='-mt-1 mx-2 text-center'>Abstammungsnachweis</h3>
+				<Select class='w-60' label='Sparte *' bind:value={pair.section} error='Pflichtfeld' on:change={updateBreeds}>
+					<option value={null}>?</option>
+					{#each sections as section }
+						<option value={section}>{section.name}</option>
+					{/each}
+				</Select>
+
+			</div>
 
 			<fieldset class='flex flex-col'>
-				<div class='flex flex-row'>
+				<div class='flex flex-row gap-x-1'>
 					<span class='w-32'> Ausstellung am </span>
 					<DateInput />
 					<span class='w-8 text-center'> in </span>
@@ -124,18 +172,13 @@
 		<main>
 			<fieldset class='flex flex-row gap-x-1'>
 				<span class='w-32'> Züchter </span>
-				<TextInput class='grow' label='Züchter'/>
+				<TextInput class='grow' label='Züchter'/><TextInput class='w-32' label='Zuchtbuch-Nr' />
 			</fieldset>
 			<fieldset class='flex flex-row gap-x-1'>
 				<span class='w-32'> Rasse </span>
-				<Select class='w-60' label='Sparte *' bind:value={certificate.section} error='Pflichtfeld' on:change={updateBreeds}>
-					<option value={null}>?</option>
-					{#each sections as section }
-						<option value={section}>{section.name}</option>
-					{/each}
-				</Select>
 
-				<Select class='w-96' label={'Rasse *'} bind:value={certificate.breed} error='Pflichtfeld' on:change={updateValues}>
+
+				<Select class='w-104' label={'Rasse *'} bind:value={pair.breed} error='Pflichtfeld' on:change={clearResults}>
 					<option value={null}>?</option>
 					{#if breeds}
 						{#each breeds as breed }
@@ -144,10 +187,10 @@
 					{/if}
 				</Select>
 
-				<Select class='grow' label={'Farbenschlag *'} bind:value={certificate.color} error='Pflichtfeld' on:change={updateValues}>
+				<Select class='grow' label={'Farbenschlag *'} bind:value={pair.color} error='Pflichtfeld'>
 					<option value={null}>?</option>
-					{#if certificate.breed}
-						{#each certificate.breed.colors as color }
+					{#if pair.breed}
+						{#each pair.breed.colors as color }
 							<option value={color}>{color.name}</option>
 						{/each}
 					{/if}
@@ -155,135 +198,82 @@
 			</fieldset>
 		</main>
 		<hr />
-		<fieldset>
+		{#if pair.breed}
 			<div class='flex flex-row'>
-				<div class='w-64'>
+				<div class='w-64 mx-4'>
 					Stamm
 				</div>
 				<div class='grow'>
 					Elterntiere
 				</div>
 			</div>
+			{#each pair.animals as animal }
+				<fieldset class='border border-gray-400 rounded p-2'>
 
-			<div class='flex flex-row'>
-				<div class='w-64 flex flex-row'><span class='mt-3 mx-1'>1.0</span><RingInput class='w-32' label='Bundesring 1.0'/> </div>
-				<div class='flex flex-col'>
-					<div class='flex flex-row'> <!-- Sire -->
-						<div class='flex flex-row'>
-							<span class='my-3 mx-1'>1.0</span>
-							<RingInput class='w-32' label='Bundesring 1.0'/>
-						</div>
-						<fieldset class='flex flex-row' on:input={gradeBrood(certificate.sire.parents.sire.brood)}>
-							<span class='w-32 mt-3 mx-1'>→ Brutleistung</span>
-							<NumberInput class='w-24' label='Eingelegte Eier' bind:value={certificate.sire.parents.sire.brood.eggs}  />
-							<span class='w-8 mt-3 mx-1'>mit</span>
-							<NumberInput class='w-24' label='Geschüpfte Küken' bind:value={certificate.sire.parents.sire.brood.hatched}  />
-							<span class='w-4 mt-3 mx-1'>=</span>
-							<output class='mt-3 mx-1 text-xl font-bold'>{certificate.sire.parents.sire.brood.grade ? dec(certificate.sire.parents.sire.brood.grade) : '?'}</output>
-						</fieldset>
-					</div>
-					<div class='flex flex-row'> <!-- Dame -->
-						<div class='flex flex-row'>
-							<span class='my-3 mx-1'>1.0</span>
-							<RingInput class='w-32' label='Bundesring 1.0'/>
-						</div>
+
+					<div class='flex flex-row'>
 						<div class='flex flex-col'>
-							<fieldset class='flex flex-row' on:input={gradeLay(certificate.sire.parents.dame.lay)}>
-								<span class='w-32 mt-3 mx-1'> → Legeleistung</span>
-								<NumberInput class='w-24' label='Legeleistung' bind:value={certificate.sire.parents.dame.lay.eggs}  />
-								<span class='w-8 mt-3 mx-1'>und</span>
-								<NumberInput class='w-24' label='SOLL Legeleistuing' value={certificate.breed ? certificate.breed.layEggs : '?'} disabled />
-								<span class='w-4 mt-3 mx-1'>=</span>
-								<output class='mt-3 mx-1 text-xl font-bold'>{certificate.sire.parents.dame.lay.grade ? dec(certificate.sire.parents.dame.lay.grade) : '?'}</output>
-							</fieldset>
-							<fieldset class='flex flex-row' on:input={gradeBrood(certificate.sire.parents.dame.brood)}>
-								<span class='w-32 mt-3 mx-1'>→ Brutleistung</span>
-								<NumberInput class='w-24' label='Eingelegte Eier' bind:value={certificate.sire.parents.dame.brood.eggs}  />
-								<span class='w-8 mt-3 mx-1'>mit</span>
-								<NumberInput class='w-24' label='Geschüpfte Küken' bind:value={certificate.sire.parents.dame.brood.hatched}  />
-								<span class='w-4 mt-3 mx-1'>=</span>
-								<output class='mt-3 mx-1 text-xl font-bold'>{certificate.sire.parents.dame.brood.grade ? dec(certificate.sire.parents.dame.brood.grade) : '?'}</output>
-							</fieldset>
+							<div class='flex flex-row p-4'>
+								<span class='w-8 mt-3 mx-2'>1.0</span>
+								<RingInput class='w-32' label='Bundesring 1.0'/>
+							</div>
+							<output class='pt-8 text-2xl text-center'>{ grade( animal.grade ) }</output>
+						</div>
+						<div class='flex flex-col gap-y-1'>
+							{#each animal.parents as parent}
+								<div class='flex flex-row border border-gray-400 rounded p-2'> <!-- parent -->
+									<div class='flex flex-row'>
+										<span class='w-8 my-3 mx-1'>{parent.sex}</span>
+										<RingInput class='w-32' label='Bundesring {parent.sex}'/>
+									</div>
+
+									<div class='flex flex-col'>
+										{#if parent.sex === '0.1'}
+<!--											<fieldset class='flex flex-row ' on:input={gradeLay(animal.parents[parentSex].lay)}> -->
+											<fieldset class='flex flex-row ' on:input={ () => gradeLay( parent.lay ) }>
+												<span class='w-32 mt-3 mx-1'> → Legeleistung</span>
+												<NumberInput class='w-24' label='Legeleistung' bind:value={parent.lay.eggs} />
+												<span class='w-8 mt-3 mx-1'>und</span>
+												<NumberInput class='w-24' label='SOLL Legeleistuing' value={pair.breed ? pair.breed.layEggs : '?'} disabled />
+												<span class='w-4 mt-3 mx-1'>=</span>
+												<output class='mt-3 mx-1 text-xl font-bold'>{parent.lay.grade}</output>
+											</fieldset>
+										{/if}
+
+										<fieldset class='flex flex-row ' on:input={ () => gradeBrood( parent.brood ) }>
+											<span class='w-32 mt-3 mx-1'>→ Brutleistung</span>
+											<NumberInput class='w-24' label='Eingelegte Eier' bind:value={parent.brood.eggs}  />
+											<span class='w-8 mt-3 mx-1'>mit</span>
+											<NumberInput class='w-24' label='Geschüpfte Küken' bind:value={parent.brood.hatched}  />
+											<span class='w-4 mt-3 mx-1'>=</span>
+											<output class='mt-3 mx-1 text-xl font-bold'>{parent.brood.grade}</output>
+										</fieldset>
+
+									</div>
+								</div>
+							{/each}
 						</div>
 					</div>
+				</fieldset>
+				<hr />
+			{/each}
+
+			<footer class='flex flex-row'>
+				<div class='grow'>
+					Datum / Unterschrift
 				</div>
-			</div>
-		</fieldset>
-		<footer>
-			<fieldset>
+				<div class='w-64 my-4 flex flex-col font-bold text-2xl'>
+					<div class='text-center'>Leistungsnote</div>
+					<strong class='m-4 text-center'>{grade(pair.grade)}</strong>
+				</div>
+			</footer>
+		{/if}
 
-			</fieldset>
-		</footer>
 
 	</Form>
 
-</article>
-
-<!--
-<main>
-
-
-	Nachweise berechnen, provisorisch
-
-	<div>Rasse</div>
-	<Form>
-		<fieldset class='flex flex-row px-2 gap-x-1'>
-			<Select class='w-60' label='Sparte *' bind:value={section} error='Pflichtfeld' on:change={updateBreeds}>
-				<option value={null}></option>
-				{#each sections as section }
-					<option value={section}>{section.name}</option>
-				{/each}
-			</Select>
-
-			<Select class='w-104' label={'Rasse *'} bind:value={breed} error='Pflichtfeld' on:change={updateValues}>
-				<option value={null}></option>
-				{#if breeds}
-					{#each breeds as breed }
-						<option value={breed}>{breed.name}</option>
-					{/each}
-				{/if}
-			</Select>
-		</fieldset>
-		<fieldset>
-			{#if section && breed}
-				{#if section.id === PIGEONS }
-					<fieldset class='border border-gray-400 rounded p-2'>
-						<div>Bewertung der Brutleistung laut AAB</div>
-						<div class='flex gap-x-2'>
-							<TextInput class='w-8' value={breed.broodGroup} label='Grp' title='Brutgruppen zur berechnung der Leistungsnote' disabled/>
-							<NumberInput class='w-20' bind:value={hasBroods} label='Bruten' title='Zahl der bruten/Nester'/>
-							<NumberInput class='w-20' bind:value={hasBroodChicks} label='Küken' title='Zahl der aufgezogene Küken'/>
-							=>
-							<TextInput class='w-20' value={grading.brood.pigeons( breed.broodGroup, hasBroods, hasBroodChicks )} label='Bewertung' title='Brutleistung' disabled/>
-						</div>
-					</fieldset>
-				{:else}
-					<div class='flex flex-col gap-y-2'>
-						<fieldset class='border border-gray-400 rounded p-2'>
-							<div>Bewertung der Legeleistung laut AAB</div>
-							<div class='flex gap-x-2'>
-								<NumberInput class='w-20' value={breed.layEggs} label='Soll legen' title='Legeleistung laut Standard' disabled/>
-								<NumberInput class='w-20' bind:value={hasLayEggs} label='Hat gelegt' title='Legeleistung in Eier / Jahr'/>
-								 =>
-								<NumberInput class='w-32 text-right' value={grading.lay( hasLayEggs, breed.layEggs )} label='Bewertung in punkte' title='Legeleistung in punkte laut H5 AAB' disabled/>
-							</div>
-						</fieldset>
-						<fieldset class='border border-gray-400 rounded p-2'>
-							<div>Bewertung der Brutleistung laut AAB</div>
-							<div class='flex gap-x-2'>
-								<NumberInput class='w-20' bind:value={hasBroodEggs} label='Eingelegt' title='Eingelegte Eier'/>
-								<NumberInput class='w-20' bind:value={hasBroodChicks} label='Küken' title='Zahl der aufgezogene Küken'/>
-								=>
-								<NumberInput class='w-32' value={grading.brood.layers( hasBroodEggs, hasBroodChicks )} label='Bewertung in punkte' title='Brutleistung in punkte laut H5 AAB' disabled/>
-							</div>
-						</fieldset>
-					</div>
-				{/if}
-			{/if}
-		</fieldset>
-	</Form>
 </main>
--->
+
 <style>
 
 </style>
