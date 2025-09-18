138e7db821de520d249bb5e652a9a7ac563c2283
eelco.jannink <e.h.a.jannink@saxion.nl>
2025-09-19 00:50:12 +0200

    Created a branch for the old rgzuchtbuch version (dd 09-2024 )

--- spa/src/lib/district/edit/Selector.svelte
+++ spa/src/lib/district/edit/Selector.svelte
@@ -46,6 +46,7 @@
 
 
 
+
 <div class='w-256 flex flex-row border border-gray-400 bg-gray-200 gap-x-4 justify-center'>
     <!--Select label="Jahr" value={year} on:change={onYear} title='Leistungsjahr'>
         {#each years as y}
